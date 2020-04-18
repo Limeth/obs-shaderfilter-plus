06c28da380f7224cb159e39be904fb3aaefe27fb
Jakub Hlusička <limeth@protonmail.com>
2020-04-18 22:01:29 +0200

    Fix legacy_stdio_definitions being required on non-windows platforms

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=legacy_stdio_definitions");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if target_os == "windows" {
+        println!("cargo:rustc-link-lib=dylib=legacy_stdio_definitions");
+    }
 }

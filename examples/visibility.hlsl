d5cf78bea170533d3828213a16da67f45262515d
Jakub Hlusička <limeth@protonmail.com>
2020-11-25 19:59:43 +0100

    Remove usage of vec4 from the visibility.hlsl example

--- examples/visibility.hlsl
+++ examples/visibility.hlsl
@@ -2,5 +2,5 @@
 // which is the number of seconds since the source was made visible.
 float4 render(float2 uv) {
     float4 image_color = image.Sample(builtin_texture_sampler, uv);
-    return lerp(vec4(0.0, 0.0, 0.0, 1.0), image_color, builtin_elapsed_time_since_shown / 5.0);
+    return lerp(float4(0.0, 0.0, 0.0, 1.0), image_color, builtin_elapsed_time_since_shown / 5.0);
 }
